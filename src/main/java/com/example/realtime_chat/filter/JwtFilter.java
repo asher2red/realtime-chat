c11f58b889a78645ff8bc9f37e9c2e508a6888e5
Peter Seo <asher2red@gmail.com>
2026-05-10 21:20:59 +0900

    feat: JWT 필터 추가 및 Spring Security 설정

---
+++ src/main/java/com/example/realtime_chat/filter/JwtFilter.java
@@ -0,0 +1,34 @@
+package com.example.realtime_chat.filter;
+
+import com.example.realtime_chat.jwt.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.replace("Bearer ", "");
+
+            String username = JwtUtil.getUsername(token);
+
+            System.out.println("인증된 사용자: " + username);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
